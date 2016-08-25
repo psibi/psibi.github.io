12d41261345de8e0b17e2ac55a6adbbebe2322c1
Sibi Prabakaran <sibi@psibi.in>
2016-08-25 22:16:20 +0530

    Just list 6 posts in the index page.

--- hakyll.hs
+++ hakyll.hs
@@ -51,8 +51,9 @@ main = hakyll $ do
         route idRoute
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
+            let sixPosts = take 6 posts
             let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
+                    listField "posts" postCtx (return sixPosts) `mappend`
                     constField "title" "Home"                `mappend`
                     defaultContext
 
